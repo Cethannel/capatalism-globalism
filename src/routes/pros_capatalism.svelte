14ce53e6c6895225b689f0926099939063169086
Ethan Nel <ethannel@protonmail.com>
2021-12-02 17:01:02 -0800

    Added Pros of Capatalism section

--- src/routes/pros_capatalism.svelte
+++ src/routes/pros_capatalism.svelte
@@ -1,19 +1,276 @@
 <script context="module">
-	export const prerender = true;
+  export const prerender = true;
 </script>
 
 <script lang="ts">
   import Writeup from '../components/Writeup.svelte';
   import Item from '../components/Item.svelte';
 
-  let baseImagePath = 'ConsCap/';
+  let baseImagePath = 'ProsCap/';
 </script>
 
 <Writeup>
-  <h1>In progress</h1>
-  <Item />
+  <Item>
+    <div slot="left">
+      <h2>
+        <a
+          href="https://fee.org/articles/extreme-poverty-rates-plummet-under-capitalism/"
+        >
+          Extreme Poverty Rates Plummet Under Capitalism</a
+        >
+      </h2>
+      <ul>
+        <li>
+          The standards of living even the poor have today would make even kings
+          from a few hundred years ago jelous. Just the fact that we have
+          running water and toilets would be miracle to them.
+        </li>
+        <li>
+          We also have made many more advances past that such as all our modern
+          technology.
+        </li>
+        <li>The GDP per person has also exploded since around the 1800s.</li>
+        <li>
+          It is also the lower classes that benefit the most from economic
+          progress, such as when your average person could now afford a car
+          thanks to Henry Ford and his production line.
+        </li>
+        <li>
+          Poverty has also plummeted following the rise of capitalism. In 1820
+          the percentage of the world living in extreme poverty (living on less
+          than $1.90 a day) was at around 90% and it is now down bellow 10%.
+        </li>
+      </ul>
+    </div>
+  </Item>
+  <Item>
+    <div slot="left">
+      <h2>
+        <a
+          href="https://fee.org/articles/what-most-americans-dont-know-about-extreme-poverty/"
+        >
+          What Most Americans Don't Know about Extreme Poverty</a
+        >
+      </h2>
+      <ul>
+        <li>
+          766,010,000, that is the number of people living in extreme poverty as
+          of 2017. That is a large number, double the population of the USA.
+        </li>
+        <li>
+          However 6,412,820,000 is another number, that is how many people
+          /aren't/ living in extreme poverty.
+        </li>
+        <ul>
+          <li>
+            That number has roughly doubled since 1987, in just one generation.
+          </li>
+          <li>
+            To be fare if your raise the threshold to 3.00$ a day that number
+            looses about 1 billion people.
+          </li>
+          <li>
+            <a
+              href="https://ourworldindata.org/no-matter-what-global-poverty-line"
+              >However even if we look at up to people living on less than
+              $10.00 a day the number has fallen and is continuing to do so.</a
+            >
+          </li>
+        </ul>
+        <li>To show a good example of this just look at China</li>
+        <ul>
+          <li>
+            In 1981 over 88% of the population lived in extreme poverty which is
+            around 878 million people which is more than the total number of
+            people in the world living in extreme poverty in 2013.
+          </li>
+          <li>
+            The rate rapidly fell to 61% in 1987, 41% in 1999, 15% in 2008, and
+            just 1.85% in 2013
+          </li>
+          <li>
+            There is no concrete data from prior to 1980, but it is estimated
+            that it was around the same since the 19th century.
+          </li>
+          <ul>
+            <li>
+              This rapid change was due to China leaving communism and moving
+              towards more of a free market.
+            </li>
+            <li>
+              This started in the 1970s, but only really got started in the 80s
+              and 90s.
+            </li>
+          </ul>
+        </ul>
+      </ul>
+    </div>
+    <div slot="right">
+      <img
+        src="{baseImagePath}distribution-of-population-poverty-thresholds.svg"
+        alt="test"
+      />
+    </div>
+  </Item>
+  <Item>
+    <div slot="left">
+      <h2>
+        <a
+          href="https://fee.org/articles/free-enterprise-is-the-worlds-only-successful-strategy-for-reducing-poverty/"
+          >Free Enterprise Is the World's Only Successful Strategy for Reducing
+          Poverty</a
+        >
+      </h2>
+      <ul>
+        <li>
+          In the past grinding poverty was the norm, but now it is the
+          exception.
+        </li>
+        <li>
+          As markets have become more free people have become more prosperous.
+        </li>
+        <li>This shows how extreme poverty has been massively reduced.</li>
+      </ul>
+    </div>
+    <div slot="right">
+      <img
+        src="https://fee.org/media/24629/img2.gif?width=600&height=600"
+        alt="show"
+      />
+    </div>
+  </Item>
+  <Item>
+    <div slot="left">
+      <h2>
+        <a href="https://fee.org/articles/equality-and-capitalism/"
+          >Equality and Capitalism</a
+        >
+      </h2>
+      <ul>
+        <li>Under Capatalism resources are distrobuted much more equally.</li>
+        <li>
+          If you were to bring someone from the 1700s to today and show them a
+          rich persons house they would be amazed, but not by things that they
+          have that others don't, but by common things.
+        </li>
+        <ul>
+          <li>
+            They would be astonished by machines that wash and dry clothes for
+            you, or even the fact that people have many sets of clean cloths.
+          </li>
+          <li>
+            Also something as simple as a fridge in the house would astonish
+            them not to mention ovens and microwaves.
+          </li>
+          <li>
+            As well as things like being able to go to the toilet without either
+            going to an outhouse or having to deal with a chamber pot.
+          </li>
+          <li>
+            Being able to listen to any music or watch any movie ever made from
+            the confort of your own home whenever you want.
+          </li>
+          <li>
+            To something as simple as hot and cold running water that is so
+            cheap it is basically free.
+          </li>
+        </ul>
+        <li>
+          These things used to only be available to kings and even then this
+          would be luxury to them.
+        </li>
+        <li>
+          These are massive improvements in human society that reduces the
+          divide between the rich and the poor.
+        </li>
+        <li>
+          The material differences of the poor and the rich have massively
+          decreased under capatalism.
+        </li>
+      </ul>
+    </div>
+  </Item>
+  <Item>
+    <div slot="left">
+      <h2>
+        <a
+          href="https://www.cato.org/policy-report/julyaugust-2013/why-capitalism-awesome#"
+          >Why Capitalism Is Awesome</a
+        >
+      </h2>
+      <ul>
+        <li>
+          Many inovations go unnoticed as they are small or not properly
+          visible.
+        </li>
+        <ul>
+          <li>
+            These are things like small parts being slightly changed on
+            something like a shelf which doesn't change it too much, but results
+            in a reduction of weight which makes it cost less.
+          </li>
+          <li>
+            These changes take to time to be visible and reduce the price or
+            increase the quality by a significant amount, but over a long enough
+            timeline they will.
+          </li>
+          <li>This is also shown with Pizza.</li>
+          <ul>
+            <li>
+              Pizza has changed much over the years from better methods of
+              preparing it, to making it easier to freeze or transport.
+            </li>
+            <li>
+              These changes are tiny, but add up the Pizza we eat now, which
+              tastes better, is delivered faster and looks better.
+            </li>
+          </ul>
+          <li>
+            There are infinitly many variables and processes that go into making
+            something as simple as a pencil. All these can be slightly tweaked
+            and changed to make it better and cheaper.
+          </li>
+        </ul>
+      </ul>
+    </div>
+  </Item>
+  <Item>
+    <div slot="left">
+      <h2>Other thouts</h2>
+      <ul>
+        <li>
+          Countries that have moved away from capitalism have increased their
+          number of poor and lowered the standard of living of their citizens.
+        </li>
+        <ul>
+          <li>
+            This is shown in Venezuela where it used to be one of the richest
+            countries is South America and is now one of the poorest.
+          </li>
+          <li>
+            After they voted in a socialist leader and he closed down and made
+            public most of the economy life went down hill.
+          </li>
+          <li>
+            It is now easier to buy essential supplies like toilet paper and
+            food in prisons than in normal stores.
+          </li>
+        </ul>
+      </ul>
+    </div>
+    <div slot="right">
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.redd.it%2F93x2hilcr5e11.jpg" alt="">
+      <p>
+        (This image shows the same shop keeper before and after communism came to Venezuela)
+      </p>
+    </div>
+  </Item>
 </Writeup>
 
 <style lang="scss">
   @import '../styles/Writing.scss';
+
+  h2 {
+    padding: 0;
+  }
 </style>
